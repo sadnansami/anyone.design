d813cf3ac872a5aee2d542e2fe6261d93d2f52bf
sadnansami <sami2841.shah@gmail.com>
2024-04-21 21:37:10 +0100

    Fix accidental .env variable leak & undo build push

---
+++ db/config.ts
@@ -0,0 +1,10 @@
+import { drizzle } from "drizzle-orm/libsql";
+import { createClient } from "@libsql/client";
+
+
+const turso = createClient({
+  url: import.meta.env.TURSO_DATABASE_URL,
+  authToken: import.meta.env.TURSO_AUTH_TOKEN,
+});
+
+export const db = drizzle(turso);``
